9740620ea5b34df0e576cb679a32bd479006e04a
Glauber Rodrigues Leite <glauberrleite>
2016-02-01 01:52:36 -0300

    Starting dynamic aspects

---
+++ console-theme.js
@@ -0,0 +1,10 @@
+var terminal = document.getElementById("terminal");
+
+var output = document.getElementById("output");
+
+var navItem = document.querySelectorAll("NAV > ul > li");
+
+//var navItem = document.getElementsByTagName("NAV").item(0);
+
+console.log(navItem);
+
