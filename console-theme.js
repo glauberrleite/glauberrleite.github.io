da69ed5c9adacf9aa18d27e42825ac220df7dcd3
glauberrleite <grl@ic.ufal.br>
2016-02-01 13:44:31 -0300

    Fixes in design elements

--- console-theme.js
+++ console-theme.js
@@ -4,27 +4,32 @@ var output = document.getElementById("output");
 
 var bottomTerminal = document.getElementById("bottom-terminal");
 
+var topTerminal = document.getElementById("top-terminal");
+
 var navItem = document.querySelectorAll("nav > ul > li > a");
 
 var path = "~";
 
 var i;
 
+topTerminal.innerHTML = "[user@glauberrleite " + path + "]$ ls";
+
+terminal.innerHTML = "[user@glauberrleite " + path + "]$ <span class='blinking-cursor'>_</span>";
+
 for(i = 0; i < navItem.length; i++){
 
 	navItem[i].onclick = function(){
 		var id = this.href.split("#")[1];
-		
+
 		var content = document.getElementById(id);
 
-		terminal.innerHTML = "<br>[user@glauberrleite " + path + "] clear && cd ~/" + this.innerHTML;
+		terminal.innerHTML = "<br>[user@glauberrleite " + path + "]$ cd ~/" + this.innerHTML;
 		path = "~/" + this.innerHTML;
-		terminal.innerHTML += "<br>[user@glauberrleite " + path + "] ls";
+		terminal.innerHTML += "<br>[user@glauberrleite " + path + "]$ ls";
 
 		output.innerHTML = content.innerHTML;
-//		output.style.display = "initial";
 
-		bottomTerminal.innerHTML = "[user@glauberrleite " + path + "]<span class='blinking-cursor'>_</span>";
+		bottomTerminal.innerHTML = "[user@glauberrleite " + path + "]$ <span class='blinking-cursor'>_</span>";
 	};
 }
 
